62ae3f98004b37e7079ec3749d179f9a10095de0
Bernardo Andrade <berdfandrade@gmail.com>
2024-12-19 17:51:59 -0300

    --

--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@
 // Define o caminho da fonte como uma constante
 const std::string FONT_PATH = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf";
 
+// Função para verificar se o mouse está sobre um retângulo 
+bool isMouseOver(const sf::RectangleShape& rect, const sf::RenderWindow& window) {
+
+     // Posição do mouse na janela
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+    // Posição do retângulo que será o botão
+    sf::Vector2f rectPos = rect.getPosition();
+
+    // Pegar o tamanho do botão
+    sf::Vector2f rectSize = rect.getSize();
+
+    // Retornamos a posição do mouse | em um bool
+    return mousePos.x >= rectPos.x && mousePos.x <= rectPos.x + rectSize.x &&
+       mousePos.y >= rectPos.y && mousePos.y <= rectPos.y + rectSize.y;
+}
+
 int main() {
+
+
     // Cria a janela
     sf::RenderWindow window(sf::VideoMode(800, 600), "Escrevendo Texto com SFML");
     window.setFramerateLimit(60);
@@ -16,10 +35,47 @@ int main() {
         return -1;
     }
 
-    // Configura o texto
+    // Cria o botão (retângulo)
+    sf::RectangleShape button(sf::Vector2f(200.f, 60.f)); // Tamanho do botão 
+
+    // Posição do botão
+    button.setPosition(300.f, 250.f);
+
+    // Cor do botão
+    button.setFillColor(sf::Color::Blue);
+
+    // Rótulo do botão
+    sf::Text buttonText;
+    sf::Text secondText; 
+
+    // Definições da FONT do botão
+    buttonText.setFont(font);
+    buttonText.setString("Botão");
+    buttonText.setCharacterSize(30);
+    buttonText.setFillColor(sf::Color::White);
+    buttonText.setStyle(sf::Text::Bold);
+    
+    // secondText.setFont(font);
+    // secondText.setString("Botão");
+    // secondText.setCharacterSize(30);
+    // secondText.setFillColor(sf::Color::White);
+    // secondText.setStyle(sf::Text::Bold);
+
+
+    // Centraliza o botão
+    sf::FloatRect textBounds = buttonText.getLocalBounds();
+
+    // Colocar o botão centralizado 
+    buttonText.setPosition(
+        button.getPosition().x + (button.getSize().x - textBounds.width) / 2.f,
+        button.getPosition().y + (button.getSize().y - textBounds.height) / 2.f - 5.f
+    );
+
+
+    // Configura o texto que aparece na tela.
     sf::Text text;
     text.setFont(font);                   // Define a fonte
-    text.setString("Hello, World!");         // Define o texto a ser exibido
+    text.setString("Hello, World!");      // Define o texto a ser exibido
     text.setCharacterSize(50);            // Tamanho da fonte
     text.setFillColor(sf::Color::White);  // Cor do texto
     text.setStyle(sf::Text::Bold);        // Estilo do texto (negrito)
